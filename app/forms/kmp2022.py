bb434e7c45cd860516bfd7e38f72cfbf39e70b3d
grilli <grilli@example.example>
2022-02-19 14:33:48 +0200

    Updated form script base to work with the latest changes. Added kmp 2022 form script and templates.

--- app/forms/kmp2022.py
+++ app/forms/kmp2022.py
@@ -23,8 +23,8 @@ class _Controller(FormController):
 
 
 _data_table_info = DataTableInfo(basic_model_csv_map())
-_event = Event('---', datetime(----),
-               datetime(----), -, _Form.asks_name_consent)
+_event = Event('OTiT KMP', datetime(2022, 2, 28, 12, 00, 00),
+               datetime(2022, 3, 13, 23, 59, 59), [Quota.default_quota(45, 15)], _Form.asks_name_consent)
 _module_info = ModuleInfo(_Controller, True, _form_name,
                           _event, _Form, _Model, _data_table_info)
 
